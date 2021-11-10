7034cb0c82d96b23675feed824db0c13ae92041e
Fernando Adán González <58112669+ferngonzalezp@users.noreply.github.com>
2021-11-10 14:53:32 +0100

    Add files via upload

---
+++ fourier_neural_op_pl.py
@@ -0,0 +1,317 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from utilities3 import *
+import operator
+from functools import reduce
+from functools import partial
+import pytorch_lightning as pl
+
+class SpectralConv3d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2, modes3):
+        super(SpectralConv3d, self).__init__()
+
+        """
+        3D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+        self.modes3 = modes3
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights3 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+        self.weights4 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, self.modes3, dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul3d(self, input, weights):
+        # (batch, in_channel, x,y,t ), (in_channel, out_channel, x,y,t) -> (batch, out_channel, x,y,t)
+        return torch.einsum("bixyz,ioxyz->boxyz", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        #Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfftn(x, dim=[-3,-2,-1])
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-3), x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2, :self.modes3] = \
+            self.compl_mul3d(x_ft[:, :, :self.modes1, :self.modes2, :self.modes3], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2, :self.modes3] = \
+            self.compl_mul3d(x_ft[:, :, -self.modes1:, :self.modes2, :self.modes3], self.weights2)
+        out_ft[:, :, :self.modes1, -self.modes2:, :self.modes3] = \
+            self.compl_mul3d(x_ft[:, :, :self.modes1, -self.modes2:, :self.modes3], self.weights3)
+        out_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3] = \
+            self.compl_mul3d(x_ft[:, :, -self.modes1:, -self.modes2:, :self.modes3], self.weights4)
+
+        #Return to physical space
+        x = torch.fft.irfftn(out_ft, s=(x.size(-3), x.size(-2), x.size(-1)))
+        return x
+
+class FNO3d(nn.Module):
+    def __init__(self, modes1, modes2, modes3, width):
+        super(FNO3d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        
+        input: the solution of the first 10 timesteps + 3 locations (u(1, x, y), ..., u(10, x, y),  x, y, t). It's a constant function in time, except for the last index.
+        input shape: (batchsize, x=64, y=64, t=40, c=13)
+        output: the solution of the next 40 timesteps
+        output shape: (batchsize, x=64, y=64, t=40, c=1)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.modes3 = modes3
+        self.padding = 6 # pad the domain if input is non-periodic
+        self.width = width
+        self.fc0 = nn.Linear(13, self.width)
+        # input channel is 12: the solution of the first 10 timesteps + 3 locations (u(1, x, y), ..., u(10, x, y),  x, y, t)
+
+        self.conv0 = SpectralConv3d(self.width, self.width, self.modes1, self.modes2, self.modes3)
+        self.conv1 = SpectralConv3d(self.width, self.width, self.modes1, self.modes2, self.modes3)
+        self.conv2 = SpectralConv3d(self.width, self.width, self.modes1, self.modes2, self.modes3)
+        self.conv3 = SpectralConv3d(self.width, self.width, self.modes1, self.modes2, self.modes3)
+        self.w0 = nn.Conv3d(self.width, self.width, 1)
+        self.w1 = nn.Conv3d(self.width, self.width, 1)
+        self.w2 = nn.Conv3d(self.width, self.width, 1)
+        self.w3 = nn.Conv3d(self.width, self.width, 1)
+        self.bn0 = torch.nn.BatchNorm3d(self.width)
+        self.bn1 = torch.nn.BatchNorm3d(self.width)
+        self.bn2 = torch.nn.BatchNorm3d(self.width)
+        self.bn3 = torch.nn.BatchNorm3d(self.width)
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+    def forward(self, x, T):
+        batchsize = x.shape[0]
+        size_x, size_y, size_z = x.shape[-3], x.shape[-2], x.shape[-1]
+        grid = self.get_grid(batchsize, size_x, size_y, T, x.device)
+        x = x.reshape(batchsize,size_x,size_y,1,size_z).repeat([1,1,1,T,1])
+        x = torch.cat((x,grid), dim=-1)
+        
+        x = self.fc0(x)
+        x = x.permute(0, 4, 1, 2, 3)
+        x = F.pad(x, [0,self.padding]) # pad the domain if input is non-periodic
+
+        x1 = self.conv0(x)
+        x2 = self.w0(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv1(x)
+        x2 = self.w1(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv2(x)
+        x2 = self.w2(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv3(x)
+        x2 = self.w3(x)
+        x = x1 + x2
+
+        x = x[..., :-self.padding]
+        x = x.permute(0, 2, 3, 4, 1) # pad the domain if input is non-periodic
+        x = self.fc1(x)
+        x = F.gelu(x)
+        x = self.fc2(x)
+        return x.reshape(batchsize,size_x,size_y,T)
+
+    def get_grid(self, batchsize, size_x, size_y, t, device):
+      # pad locations (x,y,t)
+      gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+      gridx = gridx.reshape(1, size_x, 1, 1, 1).repeat([batchsize, 1, size_y, t, 1])
+      gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+      gridy = gridy.reshape(1, 1, size_y, 1, 1).repeat([batchsize, size_x, 1, t, 1])
+      gridt = torch.tensor(np.linspace(0, 1, t+1)[1:], dtype=torch.float)
+      gridt = gridt.reshape(1, 1, 1, t, 1).repeat([batchsize, size_x, size_y, 1, 1])
+      return torch.cat((gridx, gridy, gridt), dim=-1).to(device)
+
+################################################################
+# fourier layer
+################################################################
+
+class SpectralConv2d_fast(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super(SpectralConv2d_fast, self).__init__()
+
+        """
+        2D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul2d(self, input, weights):
+        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        #Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfft2(x)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+        #Return to physical space
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+        return x
+
+class FNO2d(nn.Module):
+    def __init__(self, modes1, modes2, width):
+        super(FNO2d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        
+        input: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        input shape: (batchsize, x=64, y=64, c=12)
+        output: the solution of the next timestep
+        output shape: (batchsize, x=64, y=64, c=1)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width = width
+        self.fc0 = nn.Linear(12, self.width)
+        # input channel is 12: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+
+        self.conv0 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv1 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv2 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv3 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.w0 = nn.Conv1d(self.width, self.width, 1)
+        self.w1 = nn.Conv1d(self.width, self.width, 1)
+        self.w2 = nn.Conv1d(self.width, self.width, 1)
+        self.w3 = nn.Conv1d(self.width, self.width, 1)
+        self.bn0 = torch.nn.BatchNorm2d(self.width)
+        self.bn1 = torch.nn.BatchNorm2d(self.width)
+        self.bn2 = torch.nn.BatchNorm2d(self.width)
+        self.bn3 = torch.nn.BatchNorm2d(self.width)
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        size_x, size_y = x.shape[1], x.shape[2]
+
+        grid = self.get_grid(batchsize, size_x, size_y, x.device)
+        x = torch.cat((x, grid), dim=-1)
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+
+        x1 = self.conv0(x)
+        x2 = self.w0(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn0(x1 + x2)
+        x = F.gelu(x)
+        x1 = self.conv1(x)
+        x2 = self.w1(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn1(x1 + x2)
+        x = F.gelu(x)
+        x1 = self.conv2(x)
+        x2 = self.w2(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn2(x1 + x2)
+        x = F.gelu(x)
+        x1 = self.conv3(x)
+        x2 = self.w3(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn3(x1 + x2)
+
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = F.gelu(x)
+        x = self.fc2(x)
+        return x
+
+    def get_grid(self, batchsize, size_x, size_y, device):
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
+        return torch.cat((gridx, gridy), dim=-1).to(device)
+
+class FNO(pl.LightningModule):
+  def __init__(self, hparams):
+    super().__init__()
+    self.save_hyperparameters(hparams)
+    self.myloss =  LpLoss(size_average=True)
+    if self.hparams.model == '3d':
+      self.model = FNO3d(self.hparams.modes,self.hparams.modes,self.hparams.modes,self.hparams.width)
+
+    else:
+      self.model = FNO2d(self.hparams.modes,self.hparams.modes,self.hparams.width)
+
+  @staticmethod
+  def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("FNO")
+        parser.add_argument("--model", type=str, default='3d')
+        parser.add_argument("--T_in", type=int, default=10)
+        parser.add_argument("--modes", type=int, default=12)
+        parser.add_argument("--width", type=int, default=32)
+        return parent_parser
+  
+  def forward(self, x, T):
+
+    if self.hparams.model == '3d':
+      return self.model(x, T)
+
+    else:
+      pred = []
+      for t in range(T):
+        pred.append(self.model(x))
+        x = torch.cat((x[...,1:],pred[-1]), dim=-1)
+      return torch.cat(pred, dim=-1)
+
+  def training_step(self, batch, batch_idx):
+    x = batch[...,:self.hparams.T_in]
+    y = batch[..., self.hparams.T_in:]
+    pred = self(x, y.shape[-1])
+
+    loss = self.myloss(pred.reshape(pred.shape[0],-1), y.reshape(y.shape[0],-1))
+    self.log("train_loss", loss)
+    return loss
+  
+  def validation_step(self, batch, batch_idx):
+    x = batch[...,:self.hparams.T_in]
+    y = batch[..., self.hparams.T_in:]
+    pred = self(x, y.shape[-1])
+
+    loss = self.myloss(pred.reshape(pred.shape[0],-1), y.reshape(y.shape[0],-1))
+    self.log("val_loss", loss)
+    return loss
+  
+  def configure_optimizers(self):
+        opt = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        sch = torch.optim.lr_scheduler.StepLR(opt, 100, gamma=0.5, last_epoch=- 1, verbose=False)
+        return {"optimizer": opt, "lr_scheduler": sch}
